1352b2fe854854d25112f34a54d053893c484c5f
JetTsang <1109126828@qq.com>
2021-12-05 22:13:41 +0800

    解决Prettier和eslint冲突，加入editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    "plugin:prettier/recommended" //加入prettier插件
   ],
   parserOptions: {
     ecmaVersion: 2020,
